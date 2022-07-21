c231a82ddda3ed4cbb598aae620c2263b89b25e3
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-21 15:15:42 +0300

    TAX: Remove the product id property

--- PriceCalculatorKata/Product.cs
+++ PriceCalculatorKata/Product.cs
@@ -4,18 +4,6 @@ namespace PriceCalculatorKata;
 
 public class Product
 {
-    private int _productId;
-    public int ProductId
-    {
-        get => _productId;
-        set
-        {
-            if (!value.IsValid())
-                throw new ArgumentException("Invalid Product ID", $"{value}");
-            _productId = value;
-        }
-    }
-
     private string? _productName;
     public string? ProductName
     {
