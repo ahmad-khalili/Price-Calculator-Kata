a49549de97f58f2acfc8cb66532e9f235ef5c7a0
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-21 16:16:49 +0300

    TAX: Add a simple demo for the price with/without tax calculator

--- PriceCalculatorKata/Program.cs
+++ PriceCalculatorKata/Program.cs
@@ -4,6 +4,15 @@ public static class Program
 {
     public static void Main()
     {
-        
+        var book = new Product
+        {
+            ProductName = "The Little Prince",
+            UniversalProductCode = "12345",
+            Price = 20.25F
+        };
+        PriceCalculator.DisplayPrice(book);
+        PriceCalculator.TaxPercentage = 21;
+        Console.WriteLine();
+        PriceCalculator.DisplayPrice(book);
     }
 }
