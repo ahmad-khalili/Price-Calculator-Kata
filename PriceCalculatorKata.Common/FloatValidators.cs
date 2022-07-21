babc0815421fc0d6970be855f356223c53ccb7a4
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-21 14:53:14 +0300

    TAX: Add validators for the product properties

---
+++ PriceCalculatorKata.Common/FloatValidators.cs
@@ -0,0 +1,10 @@
+﻿namespace PriceCalculatorKata.Common;
+
+public static class FloatValidators
+{
+    public static bool FloatValid(this float source)
+    {
+        if (source <= 0) return false;
+        return true;
+    }
+}
--- PriceCalculatorKata/Product.cs
+++ PriceCalculatorKata/Product.cs
@@ -1,9 +1,47 @@
-﻿namespace PriceCalculatorKata;
+﻿using PriceCalculatorKata.Common;
+
+namespace PriceCalculatorKata;
 
 public class Product
 {
     public int ProductId { get; set; }
-    public string? ProductName { get; set; }
-    public string? UniversalProductCode { get; set; }
-    public float Price { get; set; }
+    private string? _productName;
+
+    public string? ProductName
+    {
+        get => _productName;
+        
+        set
+        {
+            if (!value.StringValid())
+                throw new ArgumentNullException(value ,"Invalid Product Name!");
+            _productName = value;
+        }
+    }
+
+    private string? _universalProductCode;
+
+    public string? UniversalProductCode
+    {
+        get => _universalProductCode;
+        set
+        {
+            if(!value.StringValid())
+                throw new ArgumentNullException(value ,"Invalid Product Code!");
+            _universalProductCode = value;
+        }
+    }
+
+    private float _price;
+    public float Price
+    {
+        get => _price;
+
+        set
+        {
+            if (!value.FloatValid())
+                throw new ArgumentException("Invalid Specified Price!", $"{value}");
+            _price = value;
+        }
+    }
 }
