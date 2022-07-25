2f10cff24b3701a5f148a352e6ab265dbfcebff1
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-25 17:43:00 +0300

    Cap: Add a Cap set method and incorporate Caps into calculation

--- PriceCalculatorKata/PriceCalculator.cs
+++ PriceCalculatorKata/PriceCalculator.cs
@@ -6,6 +6,7 @@ namespace PriceCalculatorKata;
 public static class PriceCalculator
 {
     private static float _totalDiscountAmount;
+    private static Cap _cap;
     public static void DisplayPrice(Product product, Constants.CombineMethod combineMethod)
     {
         var totalPrice = CalculateTotalPrice(product, combineMethod);
@@ -29,39 +30,64 @@ public static class PriceCalculator
     {
         float universalDiscountAmount = 0;
         float specialDiscountAmount = 0;
+        
         var productCode = product.UniversalProductCode;
+        
         float remainingPriceDiscounts = product.Price;
         float remainingPriceTax = product.Price;
+        
         var isAdditive = combineMethod.Equals(Constants.CombineMethod.Additive);
 
         if (SpecialDiscountCalculator.SpecialDiscountExists(productCode))
         {
             specialDiscountAmount =
                 SpecialDiscountCalculator.CalculateSpecialDiscountAmount(productCode, remainingPriceDiscounts);
-            
+
             if (!isAdditive) remainingPriceDiscounts -= specialDiscountAmount;
 
             if (SpecialDiscountCalculator.IsBeforeTax(productCode)) remainingPriceTax -= specialDiscountAmount;
         }
-
+        
         if (DiscountCalculator.HasDiscount())
         {
             universalDiscountAmount = DiscountCalculator.CalculateDiscountAmount(remainingPriceDiscounts);
-            
+
             if (!isAdditive) remainingPriceDiscounts -= universalDiscountAmount;
-            
+
             if (DiscountCalculator.IsBeforeTax()) remainingPriceTax -= universalDiscountAmount;
         }
-        
+
         var taxAmount = TaxCalculator.CalculateTaxAmount(remainingPriceTax);
+        
         _totalDiscountAmount = specialDiscountAmount + universalDiscountAmount;
+        
+        if (_cap.IsAboveCap(_totalDiscountAmount, product) && _cap.Amount != 0)
+        {
+            if (_cap.ValueType.Equals(Constants.ValueType.Percentage))
+            {
+                var discountAmount = _cap.Amount * product.Price;
+                _totalDiscountAmount = discountAmount;
+            }
+            else
+            {
+                _totalDiscountAmount = _cap.Amount;
+            }
+        }
         var totalExpenses = ExpenseCalculator.CalculateExpenses(product);
+        
         var totalPrice = product.Price - _totalDiscountAmount + taxAmount + totalExpenses;
+        
         return totalPrice;
     }
 
     private static void PrintTotalDiscountAmount(Product product)
     {
         Console.WriteLine($"Total Discount Amount: ${_totalDiscountAmount.SetPrecision(Constants.DecimalPrecision)}");
+    }
+
+    public static void SetCap(float amount, Constants.ValueType valueType)
+    {
+        _cap.Amount = amount;
+        _cap.ValueType = valueType;
     }
 }
