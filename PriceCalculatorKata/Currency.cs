6c9acfa2627849051eb5bb6dd1ba3f1a5af78bee
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-31 14:01:45 +0300

    Currency: Add a currency class to create a currency getter extension method

---
+++ PriceCalculatorKata/Currency.cs
@@ -0,0 +1,15 @@
+﻿namespace PriceCalculatorKata;
+
+public static class Currency
+{
+    public static string GetCurrency(this Product product)
+    {
+        if (product.Currency.Equals(Constants.Currency.USD)) return "USD";
+        
+        if (product.Currency.Equals(Constants.Currency.GBP)) return "GBP";
+        
+        if (product.Currency.Equals(Constants.Currency.JPY)) return "JPY";
+
+        return "NA";
+    }
+}
